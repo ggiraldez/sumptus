3857c5e81368112bf5aef41f757cf81fd944aa3f
Gustavo Giráldez <ggiraldez@manas.com.ar>
2016-03-14 22:01:10 -0300

    Add CSS and JS from MUI. Style the page a bit.

--- src/sumptus/views.clj
+++ src/sumptus/views.clj
@@ -1,28 +1,28 @@
 (ns sumptus.views
   (:require [ring.util.anti-forgery :as anti-forgery]
             [hiccup.def :refer [defhtml]]
-            [hiccup.page :refer [html5]]
+            [hiccup.page :refer [html5 include-css include-js]]
             [hiccup.form :as form]))
 
 (defhtml new-expense-form []
   (form/form-to
+   {:class "mui-panel expense-form"}
    [:post "/add-expense"]
    (anti-forgery/anti-forgery-field)
-   [:fieldset
-    [:div
-     (form/label :when "When")
-     (form/text-field {:type :date} :when)]
-    [:div
-     (form/label :category "Category")
-     (form/text-field :category)]
-    [:div
-     (form/label :description "Description")
-     (form/text-field :description)]
-    [:div
-     (form/label :amount "Amount")
-     (form/text-field {:type :number} :amount)]
-    [:div
-     (form/submit-button "Add expense")]]))
+   [:div.mui-textfield
+    (form/text-field {:type :date :required true} :when)
+    (form/label :when "When")]
+   [:div.mui-textfield
+    (form/text-field {:required true} :category)
+    (form/label :category "Category")]
+   [:div.mui-textfield
+    (form/text-field {:required true} :description)
+    (form/label :description "Description")]
+   [:div.mui-textfield
+    (form/text-field {:type :number :step 0.01 :required true} :amount)
+    (form/label :amount "Amount")]
+   [:div
+    (form/submit-button {:class "mui-btn mui-btn--primary"} "Add expense")]))
 
 (defhtml expense-table-row [{:keys [id when category description amount]}]
   [:tr {:data-expense-id id}
@@ -32,19 +32,30 @@
    [:td (format "$%.2f" amount)]])
 
 (defhtml recent-expenses-list [expense-list]
-  [:h2 "Recent expenses"]
-  [:table
+  [:table.mui-table
    [:thead
     [:tr
-     [:th "When"]
-     [:th "Category"]
-     [:th "Description"]
-     [:th "Amount"]]]
+     [:th {:data-field "when"}        "When"]
+     [:th {:data-field "category"}    "Category"]
+     [:th {:data-field "description"} "Description"]
+     [:th {:data-field "amount"}      "Amount"]]]
    [:tbody
     (for [expense expense-list] (expense-table-row expense))]])
 
 (defn home [recent-expenses]
   (html5
-   [:h1 "Sumptus"]
-   (new-expense-form)
-   (recent-expenses-list recent-expenses)))
+   [:head
+    [:title "Sumptus"]
+    [:meta {:charset "utf-8"}]
+    [:meta {:http-equiv "X-UA-Compatible" :content "IE=edge"}]
+    [:meta {:name "viewport" :content "width=device-width, initial-scale=1.0"}]]
+    (include-css "css/mui.min.css")
+    (include-css "css/sumptus.css")
+   [:body
+    [:div.mui-appbar
+     [:div.mui--text-title.mui--appbar-height.title "Sumptus"]]
+    [:div.mui-container
+     (new-expense-form)
+     (recent-expenses-list recent-expenses)
+     (include-js "js/mui.min.js")
+     (include-js "js/sumptus.js")]]))

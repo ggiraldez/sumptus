8898ddcceda5b70f6998e8d622ab6c341fa81b90
Gustavo Giráldez <ggiraldez@manas.tech>
2020-07-11 20:23:14 -0300

    Update expenses query to handle amounts bigger than 1,000

--- resources/queries/recent_expenses.sql
+++ resources/queries/recent_expenses.sql
@@ -1,5 +1,5 @@
 -- Retrieve recent expenses
-SELECT id, "when", category, description, amount
+SELECT id, "when", category, description, amount::decimal
 FROM expenses
 ORDER BY "when" DESC, id DESC
 LIMIT :count
